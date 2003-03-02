658946817ae401ea744d4081340120e49d7d1a43
jesse <unknown>
2003-03-02 04:41:30 +0000

    Changed dump_html default run-mode to be referenced by name instead of sub-ref.  This allows dump_html() to be overridden in sub-class

--- Application.pm
+++ Application.pm
@@ -1,10 +1,10 @@
-# $Id: Application.pm,v 1.30 2003/02/01 07:46:11 jesse Exp $
+# $Id: Application.pm,v 1.31 2003/03/01 23:41:30 jesse Exp $
 
 package CGI::Application;
 
 use strict;
 
-$CGI::Application::VERSION = '3.0';
+$CGI::Application::VERSION = '3.1';
 
 
 use CGI::Carp;
@@ -207,7 +207,7 @@ sub setup {
 
 	$self->start_mode('start');
 	$self->run_modes(
-		'start' => \&dump_html,
+		'start' => 'dump_html',
 	);
 }
 
