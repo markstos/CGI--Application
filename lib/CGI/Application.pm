d28b365499a7da76f7455d98f802bfdbf98c4b5e
Mark Stosberg <mark@summersault.com>
2005-10-12 01:59:45 +0000

    Changes update and version bump for 4.04 release

--- lib/CGI/Application.pm
+++ lib/CGI/Application.pm
@@ -3,7 +3,7 @@ use Carp;
 use strict;
 use Class::ISA;
 
-$CGI::Application::VERSION = '4.04_02';
+$CGI::Application::VERSION = '4.04';
 
 my %INSTALLED_CALLBACKS = (
 #	hook name          package                 sub
