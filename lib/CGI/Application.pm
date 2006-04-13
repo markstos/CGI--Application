8cfb4e3cc42f3f0213067e27c507dac1274f0143
Mark Stosberg <mark@summersault.com>
2006-04-13 02:31:39 +0000

    prepare for 4.06 release

--- lib/CGI/Application.pm
+++ lib/CGI/Application.pm
@@ -3,7 +3,7 @@ use Carp;
 use strict;
 use Class::ISA;
 
-$CGI::Application::VERSION = '4.05';
+$CGI::Application::VERSION = '4.06';
 
 my %INSTALLED_CALLBACKS = (
 #	hook name          package                 sub
